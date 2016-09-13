fab18a02513e6375e75e02de655625d02b10a440
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-12 21:58:11 -0700

    Adding method for displaying detail on the form

--- DjangoGit/GitApp/views.py
+++ DjangoGit/GitApp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+import requests
 
 # Create your views here.
+
+def profile(request):
+	my_json=[]
+	if request.method == 'POST':
+    	username = request.POST.get('user')
+		req = requests.get('https://api.github.com/users/'+ username)
+		my_json.append(json.loads(req.content))
+		parsedData=[]
+		UserData={}
+		for data in my_json:
+			userData['name']=data['name']
+			userData['blog'] = data['blog']
+        	userData['email'] = data['email']
+        	userData['public_gists'] = data['public_gists']
+        	userData['public_repos'] = data['public_repos']
+        	userData['avatar_url'] = data['avatar_url']
+        	userData['followers'] = data['followers']
+        	userData['following'] = data['following']
+
+    	parsedData.append(userData)
+    return render(request, 'app/profile.html',{'data':parsedData})
+
+	
